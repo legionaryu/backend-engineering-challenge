ce93e9333eee24b6fa256841333085ea0103d094
Vitaliano Palmieri Neto <700380+legionaryu@users.noreply.github.com>
2019-10-18 18:43:20 -0300

    Added new options
    Now the user can choose which property to calculate the average.
    And also added two flags that prints reports about language usage or client requests

--- unbabel_cli.py
+++ unbabel_cli.py
@@ -126,25 +126,20 @@ def ceiling_minute(datetime_obj):
     else:
         return datetime_obj
 
-
-def calc_moving_avg(input_file, window_size=10):
-    """Calculates the moving average, from a given window size, split by minute
-    from the earliest entry up to the latest entry
+def __get_item_list(input_file):
+    """Gets the item list from the input file
 
     Arguments:
-        input_file {sting} -- the path to the file containing the data
+        input_file {string} -- the file that will be parsed
 
-    Keyword Arguments:
-        window_size {int} -- the size of the window that will be averaged in
-                                minutes (default: {10})
+    Raises:
+        Exception: When the file is not parseable it raises an exception
 
     Returns:
-        {list} -- a list of objects containing the following properties:
-                    date (string) and average_delivery_time (float)
+        list -- the list containing the items parsed from the input_file
     """
     with open(input_file, "r") as file_obj:
         file_text = file_obj.read()
-        log_list = []
         try:
             # Try to load the file content as json
             item_list = json.loads(file_text)
@@ -163,49 +158,109 @@ def calc_moving_avg(input_file, window_size=10):
             except json.JSONDecodeError as je:
                 print(f"Error while parsing the json lines: {je}")
                 raise Exception("Parsing lines error")
+        return item_list
 
-        # Convert the Json objects into Log objects
-        for item in item_list:
-            log = TranslationLogItem(item)
-            heapq.heappush(log_list, log)
 
-        # Get the earliest log entry
-        current_log = heapq.heappop(log_list)
-        # Add it to the window list
-        logs_in_window = [current_log]
-        # Set the sum of log durations in the window list, used to save processing time
-        total_duration = current_log.duration
-        # Ceil the datetime of the earliest log to include it in the results
-        current_datetime = ceiling_minute(current_log.datetime)
+def calc_moving_avg(input_file, window_size=10, property_name="duration"):
+    """Calculates the moving average, from a given window size, split by minute
+    from the earliest entry up to the latest entry
 
-        result = []
-        while len(log_list) > 0:
-            # Check if we need to remove logs that are inside the window
-            lower_boundary = current_datetime - dt.timedelta(minutes=window_size)
-            while logs_in_window[0].datetime < lower_boundary:
-                # As the window list is sorted we don't need to search through the whole list
-                popped_log = logs_in_window.pop(0)
-                # To save processing time we remove the value from the total sum
-                total_duration -= popped_log.duration
-            # Then we run add all logs that are now in the window range
-            while current_log.datetime < current_datetime and len(log_list) > 0:
-                current_log = heapq.heappop(log_list)
-                logs_in_window.append(current_log)
-                # Add their values to the total
-                total_duration += current_log.duration
-            # Increase 1 minute to the current time
-            current_datetime += dt.timedelta(minutes=1)
-            # The average can be calculated using the variable instead of going through the list
-            average = total_duration / len(logs_in_window)
-            result.append(
-                {
-                    # Convert it to string to make it compliant with the requested output
-                    "date": current_datetime.strftime("%Y-%m-%d %H:%M:%S"),
-                    "average_delivery_time": average,
-                }
-            )
-        return result
+    Arguments:
+        input_file {sting} -- the path to the file containing the data
 
+    Keyword Arguments:
+        window_size {int} -- the size of the window that will be averaged in
+                                minutes (default: {10})
+        property_name {string} -- the property that will be averaged it can be: "nr_words" or "duration" (default: {"duration"})
+
+    Returns:
+        {list} -- a list of objects containing the following properties:
+                    date (string) and average_delivery_time (float)
+    """
+    item_list = __get_item_list(input_file)
+    log_list = []
+    # Convert the Json objects into Log objects
+    for item in item_list:
+        log = TranslationLogItem(item)
+        heapq.heappush(log_list, log)
+
+    # Get the earliest log entry
+    current_log = heapq.heappop(log_list)
+    # Add it to the window list
+    logs_in_window = [current_log]
+    # Set the sum of log chosen property in the window list, used to save processing time
+    total_sum = current_log[property_name]
+    # Ceil the datetime of the earliest log to include it in the results
+    current_datetime = ceiling_minute(current_log.datetime)
+
+    result = []
+    while len(log_list) > 0:
+        # Check if we need to remove logs that are inside the window
+        lower_boundary = current_datetime - dt.timedelta(minutes=window_size)
+        while logs_in_window[0].datetime < lower_boundary:
+            # As the window list is sorted we don't need to search through the whole list
+            popped_log = logs_in_window.pop(0)
+            # To save processing time we remove the value from the total sum
+            total_sum -= popped_log[property_name]
+        # Then we run add all logs that are now in the window range
+        while current_log.datetime < current_datetime and len(log_list) > 0:
+            current_log = heapq.heappop(log_list)
+            logs_in_window.append(current_log)
+            # Add their values to the total
+            total_sum += current_log[property_name]
+        # Increase 1 minute to the current time
+        current_datetime += dt.timedelta(minutes=1)
+        # The average can be calculated using the variable instead of going through the list
+        average = total_sum / len(logs_in_window)
+        result.append(
+            {
+                # Convert it to string to make it compliant with the requested output
+                "date": current_datetime.strftime("%Y-%m-%d %H:%M:%S"),
+                "average_delivery_time": average,
+            }
+        )
+    return result
+
+
+def get_clients_report(input_file):
+    """Get the clients report containing the list of clients and the amount of translations done
+
+    Arguments:
+        input_file {string} -- the path to the file to be parsed
+
+    Returns:
+        list -- An list of objects in the following format { client_name: {string}, translation_count: {int}}
+    """
+    item_list = __get_item_list(input_file)
+    clients = {}
+    for item in item_list:
+        log = TranslationLogItem(item)
+        try:
+            clients[log.client_name] += 1
+        except:
+            clients[log.client_name] = 1
+    return [{"client_name":client, "translation_count":count} for (client, count) in clients.items()]
+
+
+def get_languages_report(input_file):
+    """Get the languages report containing all languages pairs and theirs respective usage count
+
+    Arguments:
+        input_file {string} -- the path to the file to be parsed
+
+    Returns:
+        list -- An list of objects in the following format { source_language: {string}, target_language: {string}, translation_count: {int}}
+    """
+    item_list = __get_item_list(input_file)
+    languages = {}
+    for item in item_list:
+        log = TranslationLogItem(item)
+        lang_pair = f"{log.source_language}|{log.target_language}"
+        try:
+            languages[lang_pair] += 1
+        except:
+            languages[lang_pair] = 1
+    return [{"source_language":langs.split("|")[0], "target_language":langs.split("|")[1], "translation_count":count} for (langs, count) in languages.items()]
 
 if __name__ == "__main__":
     parser = CustomParser(
@@ -219,7 +274,33 @@ if __name__ == "__main__":
         default=10,
         help="the window size, in minutes, that will be averaged",
     )
+    parser.add_argument(
+        "--property",
+        required=False,
+        default="duration",
+        choices=["nr_words", "duration"],
+        help="the property that will be averaged",
+    )
+    parser.add_argument(
+        "--clients-report",
+        required=False,
+        action='store_true',
+        help="if this option is present it shows a report with the amount of translations made by each client instead of the default behavior"
+    )
+    parser.add_argument(
+        "--languages-report",
+        required=False,
+        action='store_true',
+        help="if this option is present it shows a report with all the language pairs used instead of the default behavior"
+    )
 
     args = parser.parse_args()
-    for avg in calc_moving_avg(args.input_file, args.window_size):
-        print(avg)
+    if args.clients_report:
+        for report in get_clients_report(args.input_file):
+            print(report)
+    elif args.languages_report:
+        for report in get_languages_report(args.input_file):
+            print(report)
+    else:
+        for avg in calc_moving_avg(args.input_file, args.window_size, args.property):
+            print(avg)
